745185e7661a65a1de204db51527da2ab5280f8f
Mehdi HAJAR <mehdi.hajar0@gmail.com>
2021-06-24 19:09:14 +0200

    test tailwind config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,13 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        blue: {
+          '500': '#D2691E',
+        },
+      }
+    },
   },
   variants: {
     extend: {},
